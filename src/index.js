6d40a32f2ed5a2cbb712599e4e32df86ad98c690
arrowks <ks15620545401@163.com>
2018-11-18 09:23:15 +0800

    update webpack demo

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import _ from 'lodash';
+// import printMe from './print.js';
+// import './style.css';
+import { cube } from './math.js';
+
+function component(){
+    //  var element = document.createElement("div");
+    //  var btn = document.createElement('button');
+
+    //  element.innerHTML = _.join(["webpack"],'');
+
+    //  btn.innerHTML = 'Click me and check the console';
+    //  btn,onclick = printMe;
+
+    //  element.appendChild(btn);
+     var element = document.createElement('pre');
+     element.innerHTML = [
+         'hello webpack',
+         '5 cubed is equal to ' + cube(5)
+     ].join('\n\n');
+     return element;
+}
+
+document.body.appendChild(component());
+// let element = component();
+// document.body.appendChild(component());
+
+// if(module.hot){
+//     module.hot.accept('./print.js',function(){
+//         console.log('Accepting the updated printMe module!');
+//         // printMe();
+//         document.body.removeChild(element);
+//         element = component();
+//         document.body.appendChild(element);
+//     })
+// }
